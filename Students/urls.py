cc9e15089bb87ae3756b46949ad30058ff8272bf
mdmahedihassanshuvo <mdmahedihassanshuvo8@gmail.com>
2024-10-30 09:38:50 +0600

    update view added

--- Students/urls.py
+++ Students/urls.py
@@ -1,10 +1,30 @@
 from django.urls import path
 from .views import (
     StudentListView,
-    StudentCreateView
+    StudentCreateView,
+    StudentDetailsView,
+    StudentUpdateView
 )
 
 urlpatterns = [
-    path('student_list/', StudentListView.as_view(), name='student_list'),
-    path('student_create/', StudentCreateView.as_view(), name='create_student')
+    path(
+        'student_list/',
+        StudentListView.as_view(),
+        name='student_list'
+    ),
+    path(
+        'student_create/',
+        StudentCreateView.as_view(),
+        name='create_student'
+    ),
+    path(
+        'student_details/<int:student_id>',
+        StudentDetailsView.as_view(),
+        name='student_details'
+    ),
+    path(
+        'student_update/<int:student_id>',
+        StudentUpdateView.as_view(),
+        name='update_student'
+    )
 ]
--- Students/views.py
+++ Students/views.py
@@ -1,8 +1,12 @@
 # from django.shortcuts import render
 from django.views.generic import (
     ListView,
-    CreateView
+    CreateView,
+    DetailView,
+    UpdateView,
+    DeleteView
 )
+from django.shortcuts import get_object_or_404
 from django.urls import reverse_lazy
 from .models import Student
 from .forms import StudentForm
@@ -22,3 +26,40 @@ class StudentCreateView(CreateView):
 
     def form_valid(self, form):
         return super().form_valid(form)
+
+
+class StudentDetailsView(DetailView):
+    model = Student
+    template_name = 'students/details.html'
+    context_object_name = 'student'
+
+    def get_object(self):
+        student_id = self.kwargs.get("student_id")
+        return get_object_or_404(
+            Student,
+            student_id=student_id
+        )
+
+
+class StudentUpdateView(UpdateView):
+    model = Student
+    template_name = 'students/update.html'
+    form_class = StudentForm
+    context_object_name = 'student'
+    success_url = reverse_lazy('student_list')
+
+    def get_object(self):
+        student_id = self.kwargs.get("student_id")
+        return get_object_or_404(Student, student_id=student_id)
+
+    def form_valid(self, form):
+        return super().form_valid(form)
+
+
+class StudentDeleteView(DeleteView):
+    model = Student
+    success_url = reverse_lazy('student_list')
+
+    def delete(self):
+        student_id = self.kwargs.get("student_id")
+        return get_object_or_404(Student, student_id=student_id)
